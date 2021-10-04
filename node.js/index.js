2ad0ab98eabf0f2122793ff9de84e27c54bb5004
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-10-05 01:08:31 +0900

    Update index.js

--- node.js/index.js
+++ node.js/index.js
@@ -1,18 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require('body-parser');
-const { User } = require("./models/User");
+const bodyParser = require('body-parser')
+const { User } = require("./models/User")
+const cookieParser = require('cookie-parser')
+
+const config = require('./config/key')
 
 // application/x-www-form-urlencoded
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: true }))
 
 
 //application/json
-app.use(express.json());
+app.use(express.json())
+app.use(cookieParser())
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://minseon:2086@nodejsbasic-shard-00-00.ycong.mongodb.net:27017,nodejsbasic-shard-00-01.ycong.mongodb.net:27017,nodejsbasic-shard-00-02.ycong.mongodb.net:27017/nodejsbasic?ssl=true&replicaSet=atlas-iv1wzl-shard-0&authSource=admin&retryWrites=true&w=majority')
+mongoose.connect(config.mongoURI)
 .then(() => console.log('MongoDB Connected...'))
 .catch(err => console.log(err))
 
@@ -32,6 +36,32 @@ app.post('/register', (req, res) => {
     })
 })
 
+
+app.post('/login', (req, res) => {
+    // 요청된 이메일을 데이터 베이스에서 찾기
+    User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+            return res.json({ loginSuccess: false, message: "입력된 이메일에 해당하는 유저가 없습니다." })
+        }
+        // 요청된 이메일이 데이터 베이스에 있다면 비밀번호가 올바른지 확인하기
+        user.comparePassword(req.body.password, (err, isMatch) => {
+            if (!isMatch) {
+                return res.json({ loginSuccess: false, message: "비밀번호가 틀렸습니다" })
+            }
+            // 비밀번호가 올바르다면 토큰 생성하기
+            user.generateToken((err, user) => {
+                if (err) return res.status(400).send(err)
+
+                //토큰 저장하기 (쿠키, 로컬 스토리지)
+                res.cookie("x_auth, user.token")
+                    .status(200)
+                    .json({ loginSuccess: true, userId: user._id })
+            })
+        })
+    })
+})
+
+    
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
