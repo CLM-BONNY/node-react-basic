7662e5a65caba4b7199181a96da9774025b56117
minseon6371 <66056874+minseon6371@users.noreply.github.com>
2021-10-05 05:42:05 +0900

    Update index.js

--- node.js/index.js
+++ node.js/index.js
@@ -2,10 +2,10 @@ const express = require('express')
 const app = express()
 const port = 5000
 const bodyParser = require('body-parser')
-const { User } = require("./models/User")
 const cookieParser = require('cookie-parser')
-
 const config = require('./config/key')
+const { auth } = require("./middleware/auth")
+const { User } = require("./models/User")
 
 // application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }))
@@ -24,7 +24,7 @@ app.get('/', (req, res) => {
     res.send('Hello World!')
 })
 
-app.post('/register', (req, res) => {
+app.post('/api/users/register', (req, res) => {
     // 회원가입할 때 필요한 정보들을 client에서 가져오면
     // 그것들을 데이터 베이스에 넣어준다.
 
@@ -37,7 +37,7 @@ app.post('/register', (req, res) => {
 })
 
 
-app.post('/login', (req, res) => {
+app.post('/api/users/login', (req, res) => {
     // 요청된 이메일을 데이터 베이스에서 찾기
     User.findOne({ email: req.body.email }, (err, user) => {
         if (!user) {
@@ -53,7 +53,7 @@ app.post('/login', (req, res) => {
                 if (err) return res.status(400).send(err)
 
                 //토큰 저장하기 (쿠키, 로컬 스토리지)
-                res.cookie("x_auth, user.token")
+                res.cookie("x_auth", user.token)
                     .status(200)
                     .json({ loginSuccess: true, userId: user._id })
             })
@@ -61,7 +61,28 @@ app.post('/login', (req, res) => {
     })
 })
 
+app.get('api/users/auth', auth, (req, res) => {
+    // 여기까지 미들웨어를 통과해왔다는 말은 Authentication이 True라는 말
+    res.status(200).json({
+        _id: req.user._id,
+        idAdmin: req.user.role === 0 ? false : true,
+        isAuth: true,
+        email: req.user.email,
+        name: req.user.name,
+        lastname: req.user.lastname,
+        role: req.user.role,
+        image: req.user.image
+    })
+})
     
+app.get('api/users/logout', auth, (req, res) => {
+    User.findOneAndUpdate({ _id: req.user._id }, { token: "" },
+    (err, user) => {
+        if (err) return res.json({ success: false, err })
+        return res.status(200).send({ success: true })    
+    })
+})
+
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
 })
